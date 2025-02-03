70251e50cc8ac7c3150057518b9c8820718f95d4
CoderAhmet79 <tahmet@yahoo.com>
2025-02-03 23:58:36 +0100

    css errors are fixed

--- frontend/src/Components/Stats.js
+++ frontend/src/Components/Stats.js
@@ -9,9 +9,10 @@ const Stats = () => {
 
   const bringAllWords = async () => {
     try {
-      const response = await axios.get(process.env.REACT_APP_URI + "/stats");
+      const response = await axios.get(process.env.REACT_APP_URI + 'stats');
       const fetchedWords = response?.data;
       setMywords(fetchedWords);
+      console.log('aaa :'+fetchedWords)
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
@@ -21,7 +22,6 @@ const Stats = () => {
 
   useEffect(() => {
     bringAllWords();
-    console.log(mywords); 
   }, []);
   if (loading) {
     return <div className="grid place-content-center">Loading...</div>; // Render a loading indicator while fetching data
