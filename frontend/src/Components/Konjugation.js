7d3edc1dee3ab75c1b51ecf1b3005c40d8d68588
CoderAhmet79 <tahmet@yahoo.com>
2025-02-04 11:14:16 +0100

    little fixes made

--- frontend/src/Components/Konjugation.js
+++ frontend/src/Components/Konjugation.js
@@ -9,8 +9,9 @@ const Konjugation = () => {
   const bringVerbenConjugation = async (e) => {
     e.preventDefault()
     try {
-      setLoading(true)
-      const response = await axios.get("https://german-verbs.glitch.me/german-verbs-api?verb=" + dasVerb);
+      
+      setLoading(true)     
+      const response = await axios.get("https://german-verbs.glitch.me/german-verbs-api?verb=" + dasVerb.toLowerCase());
       const fetchedWords = response?.data;
       setVerben(fetchedWords.data);
     }
@@ -66,72 +67,72 @@ const Konjugation = () => {
         <div className='font-serif pl-2 border-2 rounded border-zinc-400 w-44 flex flex-col drop-shadow-xl max-md:w-full'>
           <span className='font-bold w-full text-center'>PRASENS</span>
           <ul>
-            <li className='verb-list'>Ich: <span className='verb-span'>{verben?.PRASENS?.S1}</span>  </li>
-            <li className='verb-list'>Du: <span className='verb-span'>{verben?.PRASENS?.S2}</span></li>
-            <li className='verb-list'>Er/Sie/Es: <span className='verb-span'>{verben?.PRASENS?.S3}</span></li>
-            <li className='verb-list'>Wir: <span className='verb-span'>{verben?.PRASENS?.P1}</span></li>
-            <li className='verb-list'>Ihr: <span className='verb-span'>{verben?.PRASENS?.P2}</span></li>
-            <li className='verb-list'>Sie/sie: <span className='verb-span'>{verben?.PRASENS?.P3}</span></li>
+            <li className='verb-list'>Ich:<span className='verb-span'>{verben?.PRASENS?.S1}</span>  </li>
+            <li className='verb-list'>Du:<span className='verb-span'>{verben?.PRASENS?.S2}</span></li>
+            <li className='verb-list'>Er/Sie/Es:<span className='verb-span'>{verben?.PRASENS?.S3}</span></li>
+            <li className='verb-list'>Wir:<span className='verb-span'>{verben?.PRASENS?.P1}</span></li>
+            <li className='verb-list'>Ihr:<span className='verb-span'>{verben?.PRASENS?.P2}</span></li>
+            <li className='verb-list'>Sie/sie:<span className='verb-span'>{verben?.PRASENS?.P3}</span></li>
           </ul>
         </div>
 
         <div className='font-serif pl-2 border-2 rounded border-zinc-400 w-44 flex flex-col drop-shadow-xl max-md:w-full'>
           <span className='font-bold w-full text-center'> PRATERITUM</span>
           <ul>
-            <li className='verb-list'>Ich: <span className='verb-span'>{verben?.PRATERITUM?.S1}</span>  </li>
-            <li className='verb-list'>Du: <span className='verb-span'>{verben?.PRATERITUM?.S2}</span></li>
-            <li className='verb-list'>Er/Sie/Es: <span className='verb-span'>{verben?.PRATERITUM?.S3}</span></li>
-            <li className='verb-list'>Wir: <span className='verb-span'>{verben?.PRATERITUM?.P1}</span></li>
-            <li className='verb-list'>Ihr: <span className='verb-span'>{verben?.PRATERITUM?.P2}</span></li>
-            <li className='verb-list'>Sie/sie: <span className='verb-span'>{verben?.PRATERITUM?.P3}</span></li>
+            <li className='verb-list'>Ich:<span className='verb-span'>{verben?.PRATERITUM?.S1}</span>  </li>
+            <li className='verb-list'>Du:<span className='verb-span'>{verben?.PRATERITUM?.S2}</span></li>
+            <li className='verb-list'>Er/Sie/Es:<span className='verb-span'>{verben?.PRATERITUM?.S3}</span></li>
+            <li className='verb-list'>Wir:<span className='verb-span'>{verben?.PRATERITUM?.P1}</span></li>
+            <li className='verb-list'>Ihr:<span className='verb-span'>{verben?.PRATERITUM?.P2}</span></li>
+            <li className='verb-list'>Sie/sie:<span className='verb-span'>{verben?.PRATERITUM?.P3}</span></li>
           </ul>
         </div>
 
         <div className='font-serif pl-2 border-2 rounded border-zinc-400 flex flex-col drop-shadow-xl w-fit max-md:w-full'>
           <span className='font-bold w-full text-center'> PERFEKT</span>
           <ul>
-            <li className='verb-list'>Ich: <span className='verb-span'>{verben?.PERFEKT?.S1[0]} {verben?.PERFEKT?.S1[1]}</span>  </li>
-            <li className='verb-list'>Du: <span className='verb-span'>{verben?.PERFEKT?.S2[0]} {verben?.PERFEKT?.S2[1]}</span></li>
-            <li className='verb-list'>Er/Sie/Es: <span className='verb-span'>{verben?.PERFEKT?.S3[0]} {verben?.PERFEKT?.S3[1]}</span></li>
-            <li className='verb-list'>Wir: <span className='verb-span'>{verben?.PERFEKT?.P1[0]} {verben?.PERFEKT?.P1[1]}</span></li>
-            <li className='verb-list'>Ihr: <span className='verb-span'>{verben?.PERFEKT?.P2[0]} {verben?.PERFEKT?.P2[1]}</span></li>
-            <li className='verb-list'>Sie/sie: <span className='verb-span'>{verben?.PERFEKT?.P3[0]} {verben?.PERFEKT?.P3[1]}</span></li>
+            <li className='verb-list'>Ich:<span className='verb-span'>{verben?.PERFEKT?.S1[0]} {verben?.PERFEKT?.S1[1]}</span>  </li>
+            <li className='verb-list'>Du:<span className='verb-span'>{verben?.PERFEKT?.S2[0]} {verben?.PERFEKT?.S2[1]}</span></li>
+            <li className='verb-list'>Er/Sie/Es:<span className='verb-span'>{verben?.PERFEKT?.S3[0]} {verben?.PERFEKT?.S3[1]}</span></li>
+            <li className='verb-list'>Wir:<span className='verb-span'>{verben?.PERFEKT?.P1[0]} {verben?.PERFEKT?.P1[1]}</span></li>
+            <li className='verb-list'>Ihr:<span className='verb-span'>{verben?.PERFEKT?.P2[0]} {verben?.PERFEKT?.P2[1]}</span></li>
+            <li className='verb-list'>Sie/sie:<span className='verb-span'>{verben?.PERFEKT?.P3[0]} {verben?.PERFEKT?.P3[1]}</span></li>
           </ul>
         </div>
 
         <div className='font-serif pl-2 border-2 rounded border-zinc-400 flex flex-col drop-shadow-xl w-fit max-md:w-full'>
           <span className='font-bold w-full text-center'> FUTUR1</span>
           <ul>
-            <li className='verb-list'>Ich: <span className='verb-span'>{verben?.FUTUR1?.S1[0]} {verben?.FUTUR1?.S1[1]}</span>  </li>
-            <li className='verb-list'>Du: <span className='verb-span'>{verben?.FUTUR1?.S2[0]} {verben?.FUTUR1?.S2[1]}</span></li>
-            <li className='verb-list'>Er/Sie/Es: <span className='verb-span'>{verben?.FUTUR1?.S3[0]} {verben?.FUTUR1?.S3[1]}</span></li>
-            <li className='verb-list'>Wir: <span className='verb-span'>{verben?.FUTUR1?.P1[0]} {verben?.FUTUR1?.P1[1]}</span></li>
-            <li className='verb-list'>Ihr: <span className='verb-span'>{verben?.FUTUR1?.P2[0]} {verben?.FUTUR1?.P2[1]}</span></li>
-            <li className='verb-list'>Sie/sie: <span className='verb-span'>{verben?.FUTUR1?.P3[0]} {verben?.FUTUR1?.P3[1]}</span></li>
+            <li className='verb-list'>Ich:<span className='verb-span'>{verben?.FUTUR1?.S1[0]} {verben?.FUTUR1?.S1[1]}</span>  </li>
+            <li className='verb-list'>Du:<span className='verb-span'>{verben?.FUTUR1?.S2[0]} {verben?.FUTUR1?.S2[1]}</span></li>
+            <li className='verb-list'>Er/Sie/Es:<span className='verb-span'>{verben?.FUTUR1?.S3[0]} {verben?.FUTUR1?.S3[1]}</span></li>
+            <li className='verb-list'>Wir:<span className='verb-span'>{verben?.FUTUR1?.P1[0]} {verben?.FUTUR1?.P1[1]}</span></li>
+            <li className='verb-list'>Ihr:<span className='verb-span'>{verben?.FUTUR1?.P2[0]} {verben?.FUTUR1?.P2[1]}</span></li>
+            <li className='verb-list'>Sie/sie:<span className='verb-span'>{verben?.FUTUR1?.P3[0]} {verben?.FUTUR1?.P3[1]}</span></li>
           </ul>
         </div>
 
         <div className='font-serif pl-2 border-2 rounded border-zinc-400 flex flex-col drop-shadow-xl w-fit max-md:w-full'>
           <span className='font-bold w-full text-center'> FUTUR2</span>
           <ul>
-            <li className='verb-list'>Ich: <span className='verb-span'>{verben?.FUTUR2?.S1[0]} {verben?.FUTUR2?.S1[1]}</span>  </li>
-            <li className='verb-list'>Du: <span className='verb-span'>{verben?.FUTUR2?.S2[0]} {verben?.FUTUR2?.S2[1]}</span></li>
-            <li className='verb-list'>Er/Sie/Es: <span className='verb-span'>{verben?.FUTUR2?.S3[0]} {verben?.FUTUR2?.S3[1]}</span></li>
-            <li className='verb-list'>Wir: <span className='verb-span'>{verben?.FUTUR2?.P1[0]} {verben?.FUTUR2?.P1[1]}</span></li>
-            <li className='verb-list'>Ihr: <span className='verb-span'>{verben?.FUTUR2?.P2[0]} {verben?.FUTUR2?.P2[1]}</span></li>
-            <li className='verb-list'>Sie/sie: <span className='verb-span'>{verben?.FUTUR2?.P3[0]} {verben?.FUTUR2?.P3[1]}</span></li>
+            <li className='verb-list'>Ich:<span className='verb-span'>{verben?.FUTUR2?.S1[0]} {verben?.FUTUR2?.S1[1]}</span>  </li>
+            <li className='verb-list'>Du:<span className='verb-span'>{verben?.FUTUR2?.S2[0]} {verben?.FUTUR2?.S2[1]}</span></li>
+            <li className='verb-list'>Er/Sie/Es:<span className='verb-span'>{verben?.FUTUR2?.S3[0]} {verben?.FUTUR2?.S3[1]}</span></li>
+            <li className='verb-list'>Wir:<span className='verb-span'>{verben?.FUTUR2?.P1[0]} {verben?.FUTUR2?.P1[1]}</span></li>
+            <li className='verb-list'>Ihr:<span className='verb-span'>{verben?.FUTUR2?.P2[0]} {verben?.FUTUR2?.P2[1]}</span></li>
+            <li className='verb-list'>Sie/sie:<span className='verb-span'>{verben?.FUTUR2?.P3[0]} {verben?.FUTUR2?.P3[1]}</span></li>
           </ul>
         </div>
 
         <div className='font-serif pl-2 border-2 rounded border-zinc-400 flex flex-col drop-shadow-xl w-fit max-md:w-full'>
           <span className='font-bold w-full text-center'> PLUSQUAMPERFEKT</span>
           <ul>
-            <li className='verb-list'>Ich: <span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.S1[0]} {verben?.PLUSQUAMPERFEKT?.S1[1]}</span>  </li>
-            <li className='verb-list'>Du: <span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.S2[0]} {verben?.PLUSQUAMPERFEKT?.S2[1]}</span></li>
-            <li className='verb-list'>Er/Sie/Es: <span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.S3[0]} {verben?.PLUSQUAMPERFEKT?.S3[1]}</span></li>
-            <li className='verb-list'>Wir: <span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.P1[0]} {verben?.PLUSQUAMPERFEKT?.P1[1]}</span></li>
-            <li className='verb-list'>Ihr: <span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.P2[0]} {verben?.PLUSQUAMPERFEKT?.P2[1]}</span></li>
-            <li className='verb-list'>Sie/sie: <span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.P3[0]} {verben?.PLUSQUAMPERFEKT?.P3[1]}</span></li>
+            <li className='verb-list'>Ich:<span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.S1[0]} {verben?.PLUSQUAMPERFEKT?.S1[1]}</span>  </li>
+            <li className='verb-list'>Du:<span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.S2[0]} {verben?.PLUSQUAMPERFEKT?.S2[1]}</span></li>
+            <li className='verb-list'>Er/Sie/Es:<span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.S3[0]} {verben?.PLUSQUAMPERFEKT?.S3[1]}</span></li>
+            <li className='verb-list'>Wir:<span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.P1[0]} {verben?.PLUSQUAMPERFEKT?.P1[1]}</span></li>
+            <li className='verb-list'>Ihr:<span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.P2[0]} {verben?.PLUSQUAMPERFEKT?.P2[1]}</span></li>
+            <li className='verb-list'>Sie/sie:<span className='verb-span'>{verben?.PLUSQUAMPERFEKT?.P3[0]} {verben?.PLUSQUAMPERFEKT?.P3[1]}</span></li>
           </ul>
         </div>
 
