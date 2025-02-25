cdf85fd3746a98a536c3b20c8c53004f67434bb2
CoderAhmet79 <tahmet@yahoo.com>
2025-02-25 22:37:46 +0100

    fonts editted

--- frontend/src/Components/NotFound.js
+++ frontend/src/Components/NotFound.js
@@ -3,7 +3,8 @@ import React from 'react'
 const NotFound = () => {
   return (
     <div className='grid place-items-center text-center text-5xl h-80'>
-      <b>Ooops something went wrong!!!!</b>
+      <b> Ooops something went wrong!!!!</b>
+      <a href="/" className='text-3xl text-red-600 font-semibold '>&#10173; Go home</a>
     </div>
   )
 }
