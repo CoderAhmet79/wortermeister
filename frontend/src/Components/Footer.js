f77edac2b7b9fce3f90ebc02537773b56f6322bf
CoderAhmet79 <tahmet@yahoo.com>
2025-03-02 05:56:45 +0100

    alignment poreblem solved in footer comp

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -3,9 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
    
-    <footer className='xl:absolute xl:bottom-0 max-xl:relative max-xl:bottom-0 max-md:relative left-0 right-0 w-full flex justify-center font-bold bg-sky-700 place-items-center text-zinc-50 h-12 text-center '>
+    <footer className='xl:absolute xl:bottom-0 max-[400px]:relative max-xl:absolute max-xl:bottom-0 max-md:absolute left-0 right-0 w-full flex justify-center font-bold bg-sky-700 place-items-center text-zinc-50 h-12 text-center '>
       &copy;Coder Ahmet
+      
     </footer>
+    
   )
 }
 
