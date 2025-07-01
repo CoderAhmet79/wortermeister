8358abfa6480ce26b92635c0d5c3212335453dec
CoderAhmet79 <151970115+CoderAhmet79@users.noreply.github.com>
2025-07-01 13:47:49 +0200

    Footer Component Update, fixed position problen

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
    
-    <footer className='absolute bottom-0 left-0 right-0 w-full flex justify-center 
+    <footer className='fixed bottom-0 left-0 right-0 w-full flex justify-center 
     font-bold bg-sky-700 place-items-center text-zinc-50 h-12 text-center'>
       &copy;Coder Ahmet
     
